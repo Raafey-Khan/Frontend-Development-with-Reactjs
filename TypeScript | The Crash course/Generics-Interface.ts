93be9da59bed1d90266a7f2fab325034e411c93d
Raafey-Khan <loyaalraafay@gmail.com>
2025-05-19 20:22:28 +0000

    Finished TypeScript Crash Course

--- TypeScript | The Crash course/Generics-Interface.ts
+++ TypeScript | The Crash course/Generics-Interface.ts
@@ -1,3 +1,16 @@
+interface customInterface<T1, T2> {
+    property: T1;
+    moreProperty: T2;
+    // in this way you can create your own customInterfaces
+}
 
 
-var a: string = 'learning Genrics Interface';
+const obj: customInterface<string, number> = {
+    property: "10",
+    moreProperty: 30
+}
+
+console.log(obj);
+
+
+// you can have generic type alias also
