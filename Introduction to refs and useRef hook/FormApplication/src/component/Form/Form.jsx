aa5a5347323f14ece51541356e7f0b2d4563e9a2
Raafey-Khan <loyaalraafay@gmail.com>
2025-07-02 08:23:23 +0530

    Adding more features to form applicaiton

--- Introduction to refs and useRef hook/FormApplication/src/component/Form/Form.jsx
+++ Introduction to refs and useRef hook/FormApplication/src/component/Form/Form.jsx
@@ -1,90 +1,82 @@
-import './Form.css'
+import './Form.css';
 import { useEffect, useRef, useState } from 'react';
 import validatePassword from '../../helper/passwordValidator';
 import validateEmail from '../../helper/emailValidator';
-function Form() {
 
-    const exampleRef = useRef(0);
-    const [count, setCount] = useState(0);
+function Form() {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
 
     useEffect(() => {
-        console.log(exampleRef);
-    })
-
+        console.log(emailRef.current);
+        console.log(passwordRef.current);
+    }, []);
 
     const [formValues, setFormValues] = useState({
         email: "",
         password: ""
     });
 
-
     const handleFormSubmit = (event) => {
         event.preventDefault();
         handleValidateEmail();
         handleValidatePassword();
-        
         console.log(formValues);
-
-    }
+    };
 
     const handleValidatePassword = () => {
         const password = formValues.password;
-        
-        if(!validatePassword(password)) {
-            console.log(`Password doesn't contain required parameters `);
-        document.getElementById('password-input').focus()
-        } 
-    }
+
+        if (!validatePassword(password)) {
+            if(passwordRef.current){
+                passwordRef.current.focus();
+            }
+            console.log("Password doesn't contain required parameters");
+        }
+    };
 
     const handleValidateEmail = () => {
         const email = formValues.email;
-      
+
         if (!validateEmail(email)) {
-              document.getElementById('email-input').focus()
-            console.log(`email is doen't contain required parameters`);
+            if (emailRef.current) {
+                emailRef.current.focus();
+            }
+            console.log("Email doesn't contain required parameters");
         }
-    }
+    };
 
-    return(
+    return (
         <div>
+            New Form <br />
+            <form onSubmit={handleFormSubmit}>
+                <div className="wrapper email-input-wrapper">
+                    <input
+                        ref={emailRef}
+                        id="email-input"
+                        type="text"
+                        value={formValues.email}
+                        onChange={(event) =>
+                            setFormValues({ ...formValues, email: event.target.value })
+                        }
+                    />
+                </div>
 
-        New Form
+                <div className="wrapper password-input-wrapper">
+                    <input
+                        id="password-input"
+                        type="password"
+                        value={formValues.password}
+                        onChange={(event) =>
+                            setFormValues({ ...formValues, password: event.target.value })
+                        }
+                    />
+                </div>
 
-
-        Count: {count} <br />
-        Exampleref: {exampleRef.current}  <br />
-        <button onClick={() => setCount(count+1)}>Update Count</button>  
-        <button onClick={() => exampleRef.current++} >Update Ref</button>
-
-        <form onSubmit={handleFormSubmit} >
-
-            <div className="wrapper email-input-wrapper">
-                <input 
-                id='email-input'
-                type="text" 
-                value={formValues.email} 
-                onChange={(event) => setFormValues({ ...formValues, email: event.target.value})}
-                
-                />
-            </div>
-
-            <div className="wrapper password-input-wrapper">
-                <input 
-                id='password-input'
-                type="password" 
-                value={formValues.password} 
-                onChange={(event) => setFormValues({ ...formValues, password: event.target.value})}
-                
-                />
-            </div>
-
-            <input id='submit-input' type="submit" />
-
-        </form>
-        
-        
+                <input id="submit-input" type="submit" />
+            </form>
         </div>
-    )
+    );
 }
 
 export default Form;
